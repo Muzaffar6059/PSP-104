f88af6d56c4d47a1147ced4fe00f64d8193e2d38
Muzaffar6059 <mirzamuzu1@gmail.com>
2023-12-03 21:50:35 +0500

    Add new file to print a diamond pattern

---
+++ Pyramid Pattern/Pyramid Pattern/Pyramid Pattern.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int row,num;
+
+        cout << "Enter A number: " << endl;
+        cin >> row;
+        while (row % 2 == 0)
+        {
+            cout << "please enter an odd number" << endl;
+            cin >> row;
+        }
+
+            for (int i = 0; i <= row; i++)
+            {
+                for (int j = 1; j <= row - i; j++)
+                {
+                    cout << " ";
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    cout << "*";
+                }
+                cout << endl;
+
+            }
+
+            for (int i = row - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= row - i; j++)
+                {
+                    cout << " ";
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    cout << "*";
+                }
+                cout << endl;
+
+            }
+
+}
