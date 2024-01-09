1c98288d84b68296dd05e223df17d0032ce77d7a
Muzaffar6059 <mirzamuzu1@gmail.com>
2024-01-10 01:45:47 +0500

    update merge and sorting

--- Merge and sort of 2 array/Merge and sort of 2 array/Merge and sort of 2 array.cpp
+++ Merge and sort of 2 array/Merge and sort of 2 array/Merge and sort of 2 array.cpp
@@ -4,8 +4,8 @@
 
    // Function to perform bubble sort on an array
    void bubbleSort(int arr[], int size) {
-       for (int i = 0; i < 2*size - 1; ++i) {
-           for (int j = 0; j < 2*size - i - 1; ++j) {
+       for (int i = 0; i < size - 1; ++i) {
+           for (int j = 0; j < size - i - 1; ++j) {
                if (arr[j] > arr[j + 1]) {
                    // Swap arr[j] and arr[j + 1]
                    int temp = arr[j];
@@ -18,18 +18,25 @@
 
    int main() {
      const int N = 4;
+     const int N2 = 3;
      int arr1[N] = {1, 5, 18, 10};
-     int arr2[N] = {2, 4, 6, 9};
-     int arr3[2*N];
+     int arr2[N2] = {2, 4, 9};
+     int arr3[N+N2];
 
-     for (int i = 0; i < N; i++) {
+     const int size = max(N, N2);
+
+     for (int i = 0; i < size; i++) {
        arr3[2*i] = arr1[i];
+       if ((2 * i + 1) < (N + N2)) {
+
        arr3[2*i + 1] = arr2[i];
+       }
      }
 
-     bubbleSort(arr3, N);
+     const int tN = N + N2;
+     bubbleSort(arr3, tN);
 
-     for (int i = 0; i < 2*N; i++) {
+     for (int i = 0; i < (N+N2); i++) {
        cout << arr3[i] << " ";
      }
      return 0;
